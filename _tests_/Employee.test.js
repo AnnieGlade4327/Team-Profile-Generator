8c3c75203116cfdf607361a9dc89e215d7abc18b
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-28 10:34:08 -0600

    tested node.js

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,34 @@
+  
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Some One', '1234', 'doe@doe.com');
+
+    expect(employee.name).toBe('Some One');
+    expect(employee.id).toBe('1234');
+    expect(employee.email).toBe('doe@doe.com');
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('Some One', '1234', 'doe@doe.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('Some One'));
+});
+
+test("gets employee's ID", () => {
+    const employee = new Employee('Some One', '1234', 'doe@doe.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining('1234'));
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('Some One', '1234', 'doe@doe.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('doe@doe.com'));
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('Some One', '1234', 'doe@doe.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
+});
