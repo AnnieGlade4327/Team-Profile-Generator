952defbee79812db47f61ae36905b0fbcda75de5
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-28 18:06:38 -0600

    finals

--- src/template.js
+++ src/template.js
@@ -1,7 +1,6 @@
-const generateCards = teamData => {
- 
-    const manager = teamData.manager.map(function(job) {
-        let managerHtml = `
+const generateCards = (teamData) => {
+  const manager = teamData.manager.map(function (job) {
+    let managerHtml = `
         <div class="card" style="width: 250px;">
             <h2>${job.name}</h2>
             <h4>Manager<h4>
@@ -9,12 +8,12 @@ const generateCards = teamData => {
             <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
             <p>Office Number: ${job.office}</p>
         </div>
-        `
-        return managerHtml
-    });
+        `;
+    return managerHtml;
+  });
 
-    const engineer = teamData.engineer.map(function(job) {
-        let engineerHtml = `
+  const engineer = teamData.engineer.map(function (job) {
+    let engineerHtml = `
         <div class="card" style="width: 225px;">
             <h2>${job.name}</h2>
             <h4>Engineer<h4>
@@ -22,12 +21,12 @@ const generateCards = teamData => {
             <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
             <p> Github: <a href="https://github.com/${job.github}" target="_blank">${job.github}</a></p>
         </div>
-        `
-        return engineerHtml
-    })
+        `;
+    return engineerHtml;
+  });
 
-    const intern = teamData.intern.map(function(job) {
-        let internHtml = `
+  const intern = teamData.intern.map(function (job) {
+    let internHtml = `
         <div class="card" style="width: 220px;">
             <h2>${job.name}</h2>
             <h4>Intern<h4>
@@ -35,20 +34,20 @@ const generateCards = teamData => {
             <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
             <p> School: ${job.school}</p>
         </div>
-        `
-        return internHtml
-    })
-    return [manager,engineer,intern]
-}
+        `;
+    return internHtml;
+  });
+  return [manager, engineer, intern];
+};
 
-module.exports = templateData => {
-    return `
+module.exports = (templateData) => {
+  return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <title>My Team</title>
         </head>
         <body>
@@ -56,8 +55,10 @@ module.exports = templateData => {
             <h1 class="text-center">The Team</h1>
             </header>
             ${generateCards(templateData)}
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         </body>
         </html>    
-        `
-}
-
+        `;
+};
